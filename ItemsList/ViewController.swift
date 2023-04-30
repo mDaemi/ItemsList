301076cb83d9a51e4f6cce4868f0463cd0c2d204
Maedeh Daemi <maede.daemi@gmail.com>
2023-04-30 09:53:59 +0200

    Initial commit

---
+++ ItemsList/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ItemsList
+//
+//  Created by MDA on 30/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
