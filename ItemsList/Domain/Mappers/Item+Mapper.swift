aa6135fdea3e6f987a399aa9e70aa0dd5d0315ae
Maedeh Daemi <maede.daemi@gmail.com>
2023-05-04 18:36:48 +0200

    Add NSNumberFormatter for price (In the Item extension, toPresentation function)

--- ItemsList/Domain/Mappers/Item+Mapper.swift
+++ ItemsList/Domain/Mappers/Item+Mapper.swift
@@ -11,12 +11,19 @@ extension Item {
     func toPresentation() -> ItemUIModel {
         return ItemUIModel(title: self.title,
                            description: self.description,
-                           price: (self.price == nil) ? "" : String(self.price!) + " €",
+                           price: (self.price == nil) ? "" : numberFormatter(on: self.price!),
                            images_url: self.images_url,
                            creation_date: (self.creation_date == nil) ? "" :
                             DateFormatter.europeanFormat.string(from: self.creation_date!),
                            is_urgent: self.is_urgent,
                            category_name: "",
                            category_id: self.category_id)
+    }
+    
+    private func numberFormatter(on price: Double) -> String {
+        let currencyFormatter = NumberFormatter()
+        currencyFormatter.numberStyle = .currency
+        currencyFormatter.locale = Locale(identifier: "es_ES")
+        return currencyFormatter.string(from: NSNumber(value: price)) ?? ""
     }
 }
